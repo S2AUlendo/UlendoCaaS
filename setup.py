6877827781c20a67d322b426cfc1e0013ba9cb2a
Ian Ku <ian@ulendo.io>
2024-04-05 18:17:10 -0400

    remove extra quote in string setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.1.3"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """"Automatically Configure Input Shaping and other advanced algorithms""""
+plugin_description = """Automatically Configure Input Shaping and other advanced algorithms"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Ulendo Technologies, Inc."
